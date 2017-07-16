7e3aa326bd2eda997aa8233623f76b8a330d11bc
Matt Sullivan <matt.j.sullivan@gmail.com>
2017-07-16 00:51:23 -0700

    Fixed formatting in assembler files

--- hello_world_asm/hello_world_1.asm
+++ hello_world_asm/hello_world_1.asm
@@ -3,13 +3,13 @@
 ; to compile with z88dk to a tap file, use
 ; z80asm +zx hello_world.asm
 
-		ld a, 2			    ; upper screen (use 1 for lower)
-		call 5633		    ; open channel
+    ld a, 2                     ; upper screen (use 1 for lower)
+    call 5633                   ; open channel
 
-        ld de, string		; address of string
-		ld bc, eostr-string ; length of string to print
-		call 8252		    ; print the string
+        ld de, string           ; address of string
+    ld bc, eostr-string         ; length of string to print
+    call 8252                   ; print the string
         ret
 
-string:	defb "Hello world"	; string to print
+string:    defb "Hello world"   ; string to print
 eostr:
