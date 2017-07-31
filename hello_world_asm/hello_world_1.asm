4390255a9cff07b3de6fe14a0871112dc108e8e2
Matt Sullivan <matt.j.sullivan@gmail.com>
2017-07-30 21:44:09 -0700

    Added more comments

--- hello_world_asm/hello_world_1.asm
+++ hello_world_asm/hello_world_1.asm
@@ -2,14 +2,22 @@
 
 ; to compile with z88dk to a tap file, use
 ; z80asm +zx hello_world.asm
+; make sure you're using a recent version of z88dk - tested with nightly build
+; which can be downloaded from http://nightly.z88dk.org/
+
+; to run this program using Spin, assemble, push into memory @ location 50000
+; and then run the following basic
+
+; 10 randomize usr 50000
 
     ld a, 2                     ; upper screen (use 1 for lower)
     call 5633                   ; open channel
 
-        ld de, string           ; address of string
+    ld de, string           ; address of string
     ld bc, eostr-string         ; length of string to print
     call 8252                   ; print the string
-        ret
+    ret
 
-string:    defb "Hello world"   ; string to print
+string:
+    defb "Hello world"   ; string to print
 eostr:
--- hello_world_asm/hello_world_2.asm
+++ hello_world_asm/hello_world_2.asm
@@ -1,20 +1,29 @@
 ; Simple hello world with control codes that repeated prints
 
 ; to compile with z88dk to a tap file, use
-; z80asm +zx hello_world_2.asm
+; z80asm +zx hello_world.asm
+; make sure you're using a recent version of z88dk - tested with nightly build
+; which can be downloaded from http://nightly.z88dk.org/
 
-        ; org 50000         ; origin point of code in memory
+; to run this program using Spin, assemble, push into memory @ location 50000
+; and then run the following basic
 
-        ld a, 2             ; upper screen (use 1 for lower)
-        call 5633           ; open channel
+; 10 randomize usr 50000
 
-loop:    ld de, string      ; address of string
-        ld bc, eostr-string ; length of string to print
-        call 8252           ; print the string
-        jp loop             ; repeat until screen is filled
+    ; org 50000         ; origin point of code in memory
 
-string:    defb 16, 3       ; ink colour
-        defb 17, 4          ; paper colour
-        defb "Hello world"  ; string to print
-        defb 13             ; RETURN
+    ld a, 2             ; upper screen (use 1 for lower)
+    call 5633           ; open channel
+
+loop:
+    ld de, string       ; address of string
+    ld bc, eostr-string ; length of string to print
+    call 8252           ; print the string
+    jp loop             ; repeat until screen is filled
+
+string:
+    defb 16, 3          ; ink colour
+    defb 17, 4          ; paper colour
+    defb "Hello world"  ; string to print
+    defb 13             ; RETURN
 eostr:
