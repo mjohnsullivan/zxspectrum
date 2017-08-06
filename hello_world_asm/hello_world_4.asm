614661808754f420c0a4941eca02a3993a33cad9
Matt Sullivan <matt.j.sullivan@gmail.com>
2017-08-06 13:55:44 -0700

    hello world asm example 4 now working

--- hello_world_asm/hello_world_4.asm
+++ hello_world_asm/hello_world_4.asm
@@ -19,6 +19,10 @@ defc print     = $203C  ; print (8252 decimal)
 defc clear     = $0DAF  ; clear (3503 decimal)
 defc screencol = $5C8D  ; set ink and paper colors (23693 decimal)
 
+; define some constants
+defc screenwidth  = 32  ; char width of the screen
+defc screenheight = 22  ; char height of the screen
+
 ; set the screen and ink colour
     ld a, 49            ; blue ink (1) on yellow paper (6*8)
     ld (screencol), a   ; set the screen colours
@@ -32,33 +36,53 @@ defc screencol = $5C8D  ; set ink and paper colors (23693 decimal)
     ld a, 2             ; upper screen (use 1 for lower)
     call opench         ; open channel
 
-; second tick
-;    call setxy
-;    ld a, 1
-;    ld b, 2
-;    ld de, eostr-2
-;    ld bc, eostr-eostr+2
-;    call print
-;    call shortpause
 
-; third tick
-loop:
-    ld d, 0
+; scrolling text vertically loop
+xloop:
+    ; set ypos to 0
+    ld a, 0
+    ld (ypos), a
+    ; set offset to zero
+    ld (offset), a
+    ; set counter to 1
+    ld a, 1
+    ld (counter), a
+
+    ; increment xpos
+    ld a, (xpos)
+    inc a
+    ld (xpos), a
+
+    ; if xpos > screenheight, end of vertical scrolling
+    ld a, (xpos)
+    ld b, a
+    ld a, screenheight
+    cp b
+    jr z, end
+
+
+; scrolling text horizontally loop
+yloop:
+    ; if counter > screen width, string at right side of screen
     ld a, (counter)
+    ld b, screenwidth+1
+    cp b
+    jr nc, strend
 
-    ; if a < strlen, e=a && xpos=0
-    ; if a >= strlen, e=strlen && xpos++
-    ld b, eostr-string+1 ; length of string
+    ; if counter < string length, string is at left side of screen
+    ld a, (counter)       ; loop position stored in counter
+    ld b, eostr-string+1  ; length of string
     cp b
     jr c, strstart
-    jr nc, strmiddle
+    ; if counter >= string length < screen width. string in middle of screen
+    jr nc, strmiddle      ; string fully on screen
     jr main
 
-strstart:
+strstart:                 ; string is partially on screen, add new char
     ld e, a
     jr main
 
-strmiddle:
+strmiddle:              ; string is fully on screen, scroll to right
     ; clear the current char at pos
     call setxy
     ld a, 32            ; code for SPACE
@@ -67,13 +91,39 @@ strmiddle:
     ld a, (ypos)
     inc a
     ld (ypos), a
-    ; set e to length od string
+    ; set e to length of string
     ld e, eostr-string
+    jr main
+
+strend:                 ; string is at end of screen, remove one char
+    ; clear the current char at pos
+    call setxy
+    ld a, 32            ; code for SPACE
+    rst 16              ; print SPACE
+    ; increment ypos
+    ld a, (ypos)
+    inc a
+    ld (ypos), a
+    ; set e to length of string - (counter - screenwidth)
+    ld a, (counter)
+    ld e, a
+    ld a, eostr - string + screenwidth
+    sub a, e
+    ld e, a
+    jr main
 
 main:
+    ; if ypos > screenwidth, end of horizontal scrolling
+    ld a, (ypos)
+    ld b, a
+    ld a, screenwidth
+    cp b
+    jr z, xloop
+
     call setxy
     ; calculate start pos of string to print
     ld hl, eostr
+    ld d, 0
     sbc hl, de
     ld (offset), hl
     ld de, (offset)
@@ -91,11 +141,9 @@ main:
     inc a
     ld (counter), a
 
-    ; compare counter to 10
-    ld b, 20
-    ld a, (counter)
-    cp b
-    jr nz, loop
+    jr yloop
+
+end:
     ret
 
 ;
@@ -111,7 +159,7 @@ setxy:
     ret                 ; position set, return
 
 shortpause:
-    ld b, 10            ; time to pause (1/50 sec)
+    ld b, 2             ; time to pause (1/50 sec)
 shortpauseloop:
     halt                ; wait for interrupt
     djnz shortpauseloop ; repeat if b not 0
@@ -129,7 +177,7 @@ string:
 eostr:                  ; end of string marker
 
 xpos:
-  defb 0
+  defb -1
 ypos:
   defb 0
 offset:
